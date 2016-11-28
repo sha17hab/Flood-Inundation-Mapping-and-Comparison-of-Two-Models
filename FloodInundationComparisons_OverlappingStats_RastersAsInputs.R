22a547f0e13a32cdd496fd7d8e359a5d55105d10
Shahab Afshari <safshar00@citymail.cuny.edu>
2016-11-27 22:44:48 -0500

    Update FloodInundationComparisons_OverlappingStats_RastersAsInputs.R

--- FloodInundationComparisons_OverlappingStats_RastersAsInputs.R
+++ FloodInundationComparisons_OverlappingStats_RastersAsInputs.R
@@ -48,36 +48,68 @@ library("sp")
 rm(list=setdiff(ls(),c("S1","myImagePlot")))
 #############################################################
 # The Script:
+
 ## 1)
+setwd("/Volumes/RAID1-2TB/virtualbox_shahab/Paper - MultiModelComaprison/NFIE_data/NWM_Geospatial_data/Data To Be Shared with the Team/InundationResults_Rasters/")
+
 #### 1-1) Importing Inundation Depth Raster Images based on 
 #### the percentage of wetted, dry, and NA cells. 
 
 #load raster in an R object called 'DEM'
-DEM_Flood1_0 = raster("Model-1.tif")
-DEM_Flood2_0 = raster("Model-2.tif")
-DEM_Terrain = raster("TerrainModel.tif")
+#raster("Model-1.tif")
+DEM_Flood1_0 = raster("HECRAS2D/100yrFlood/MaxDepth_NED_25m_LMn_DT1min_100yr.tif") 
+#raster("Model-2.tif")
+DEM_Flood2_0 = raster("AutoRoute_Rasters/Depth_10mned_WholeTusc_100yr_hybrid_clip.tif") 
+DEM_Flood2_0 = raster("HANDFloodmap_Xing/HANDFloodmap/NED/FP_100.tif") 
+#raster("TerrainModel.tif")
+DEM_Terrain = raster("/Volumes/RAID1-2TB/virtualbox_shahab/Paper - MultiModelComaprison/NFIE_data/NWM_Geospatial_data/Data To Be Shared with the Team/DEMs/Inputs/10-19-2016-Shahab-10mNEDLev/10-19-2016-Shahab-UTM-10mNEDLev-meter.tif") 
 
 ### NOTE: the extent of "TerrainModel" raster is the baseline, that is, 
 ###       the cells of "Model-1" and "Model-2" which are being mapped
 ###       beyond the baseline extent will be omitted/clipped
 
+#### 1-2) cell resolution and projection comparison and adjustment; 
+#### The coordinate refrence system of "Model-1" and "Model-2" 
+#### will be compared with that of the "TerrainModel". 
+#### "Model-1" and "Model-2" rasters (or their projected forms) will be resampled
+#### to the spatial resolution similar to the resolution "TerrainModel" raster. 
+DEM_Flood1 = DEM_Flood1_0
+if (crs(DEM_Flood1,asText=T)!=crs(DEM_Terrain,asText=T)) {
+  DEM_Flood1 = projectRaster(from = DEM_Flood1,to=DEM_Terrain)
+} else if (res(DEM_Flood1)[1]!=res(DEM_Terrain)[1]) {
+  DEM_Flood1 = resample(DEM_Flood1,DEM_Terrain,method="bilinear")
+} 
+
+DEM_Flood2 = DEM_Flood2_0
+if (crs(DEM_Flood2,asText=T)!=crs(DEM_Terrain,asText=T)) {
+  DEM_Flood2 = projectRaster(from = DEM_Flood2,to=DEM_Terrain);print("1")
+} else if (res(DEM_Flood2)[1]!=res(DEM_Terrain)[1]) {
+  DEM_Flood2 = resample(DEM_Flood2,DEM_Terrain,method="bilinear");print("2")
+} 
+
+#### 1-3) setting the coordinates of the origin of the "TerrainModel" raster as 
+#### the coordinates of the point of origin of "Model-1" and "Model-2" rasters 
+#### (or their projected forms)
+if ((origin(DEM_Flood1)[1] != origin(DEM_Terrain)[1]) | (origin(DEM_Flood1)[2] != origin(DEM_Terrain)[2])) 
+  origin(DEM_Flood1) = origin(DEM_Terrain)
+if ((origin(DEM_Flood2)[1] != origin(DEM_Terrain)[1]) | (origin(DEM_Flood2)[2] != origin(DEM_Terrain)[2])) 
+  origin(DEM_Flood2) = origin(DEM_Terrain)
+rm(DEM_Flood2_0);rm(DEM_Flood1_0)
+
 # cell resolution comparison; raster image having lower resolution will be resampled
 # to the spatial resolution similar to finer resolution raster. 
-if (res(DEM_Flood1_0)[1]!=res(DEM_Flood2_0)[1]) {
-  if (res(DEM_Flood1_0)[1]<res(DEM_Flood2_0)[1]) {
-    DEM_Flood2 = resample(DEM_Flood2_0,DEM_Flood1_0,method='ngb')
-  } else DEM_Flood1 = resample(DEM_Flood1_0,DEM_Flood2_0,method='ngb')
-} else DEM_Flood2 = DEM_Flood2_0; DEM_Flood1 = DEM_Flood1_0
+# if (res(DEM_Flood1_0)[1]!=res(DEM_Flood2_0)[1]) {
+#   if (res(DEM_Flood1_0)[1]<res(DEM_Flood2_0)[1]) {
+#     DEM_Flood2 = resample(DEM_Flood2_0,DEM_Flood1_0,method="bilinear")
+#   } else DEM_Flood1 = resample(DEM_Flood1_0,DEM_Flood2_0,method="bilinear")
+# } else DEM_Flood2 = DEM_Flood2_0; DEM_Flood1 = DEM_Flood1_0
 
-#calculate and save the min and max values of the raster to the raster object
-DEM_Flood1 = setMinMax(DEM_Flood1)
-DEM_Flood2 = setMinMax(DEM_Flood2)
-DEM_Terrain = setMinMax(DEM_Terrain)
-rm(DEM_Flood1_0);rm(DEM_Flood2_0)
 
 #############################################################
 ## 2)
 ####### Functions to be used in image processing ###########
+# fun0: given the raster image (representing flood depth), it will convert
+# cells having zero z-values (i.e. zero depth) to NA
 # fun1: given the raster image, it will convert z-values of non-NA cells
 # to 1 and NA cells to 0
 # fun2: given the raster image, it will convert z-values of non-NA cells 
@@ -86,21 +118,44 @@ rm(DEM_Flood1_0);rm(DEM_Flood2_0)
 # are classified into 0, 1, 2, and 3 categories, these functions are being
 # applied to sort out the raster cells having a particular value, e.g. 1 that is 
 # being captured by fun3_1, and exporting it a as new raster layer. 
+# fun4: given the raster image (representing flood depth): 1. it will convert
+# cells having zero z-values (i.e. zero depth) to NA, 2. converting non-NA cells 
+# to 1, and 3. NA values (where also have been added from step 1) to 0
+fun0 = function(x){ x[x==0] = NA; return(x) }
 fun1 = function(x) { x[!is.na(x)] = 1; x[is.na(x)] = 0;return(x) }
 fun2 = function(x){ x[!is.na(x)] = 0; return(x) }
 fun3_union = function(x){ x[x==1|x==2|x==3] = 1; return(x) }
 fun3_1 = function(x){ x[x==0|x==2|x==3] = NA; x[x==1] = 0; return(x) }
 fun3_2 = function(x){ x[x==0|x==1|x==3] = NA; x[x==2] = 0; return(x) }
 fun3_3 = function(x){ x[x==0|x==1|x==2] = NA; x[x==3] = 0; return(x) }
+fun4 = function(x){ x[x==0] = NA; x[!is.na(x)] = 1; x[is.na(x)] = 0; return(x) }
+
 #############################################################
 ## 3)
-#### Generating Binary and Discrete Inundation Raster Layers From Depth Rasters ###########
+# considering the "TerrainModel.tif" or "DEM_Terrain" as the extent for clipping 
+# "Model-1.tif" and "Model-2.tif" ("DEM_Flood1" and "DEM_Flood2")
+DEM_Flood1 = intersect(calc(DEM_Flood1, fun0),DEM_Terrain*0)
+DEM_Flood2 = intersect(calc(DEM_Flood2, fun0),DEM_Terrain*0)
+# calculate and save the min and max values of the raster to the raster object
+DEM_Flood1 = setMinMax(DEM_Flood1)
+DEM_Flood2 = setMinMax(DEM_Flood2)
+DEM_Terrain = setMinMax(DEM_Terrain)
 
+# DEM_Flood1 = resample(DEM_Terrain, DEM_Flood1, method="ngb")
+# DEM_Flood2 = resample(DEM_Terrain, DEM_Flood2, method="ngb")
+# DEM_Flood2 = calc(DEM_Flood2,fun4)
+
+#### Generating Binary and Discrete Inundation Raster Layers From Depth Rasters ###########
+oldw <- getOption("warn")
+options(warn = -1)
 ### 3-1) Flood raster where z-values are classified into 0, 1, 2, and 3 category
 DEM_FloodClassified = calc(DEM_Flood1, fun1)+
   calc(DEM_Flood2, fun1)*2+
   calc(DEM_Terrain,fun2) 
-
+# DEM_FloodClassified0 = calc(DEM_Flood1, fun1)+
+#   calc(DEM_Flood2, fun4)*2+
+#   calc(DEM_Terrain,fun2) 
+# plot(calc(DEM_Flood2,fun4))
 ### 3-2) Union of flood raster layers, 0 being the union and NA as non-flooded zones 
 DEM_FloodUnion = calc(DEM_FloodClassified,fun3_union)
 
@@ -114,6 +169,7 @@ DEM_FloodClassified_1 = calc(DEM_FloodClassified,fun3_1)
 ### 3-5) Unique flooded zones by a flood raster 2 while it is not flooded by flood raster 1,
 ### 0 being the floode zones and NA as non-flooded or non-intersected zones
 DEM_FloodClassified_2 = calc(DEM_FloodClassified,fun3_2)
+options(warn = oldw)
 
 ## 4) 
 ### Numerical and Visual Comparison of Models
@@ -121,11 +177,21 @@ DEM_FloodClassified_2 = calc(DEM_FloodClassified,fun3_2)
 ### 4-1) Converting Raster into Matrix
 #### Initialization for calculating inundation statistics
 S1 = as.matrix(DEM_FloodClassified)
-#quick view of the matrix (raster)
+#quick view of the matrix (raster), where cell values are
+# 0: Not flooded by both "Model-1.tif" and "Model-2.tif" ("DEM_Flood1" and "DEM_Flood2")
+# 1: Only flooded by "Model-1.tif" ("DEM_Flood1")
+# 2: Only flooded by "Model-2.tif" ("DEM_Flood2")
+# 3: Flooded both by "Model-1.tif" and "Model-2.tif" ("DEM_Flood1" and "DEM_Flood2")
 plot(DEM_FloodClassified, main="Digital Elevation Model")
 #we can look at the distribution of values in the raster too
-hist(S1, main="Distribution of elevation values", 
+S1_dum = S1[which(!is.na(S1)==T)]
+library(lattice)
+histogram(S1[which(!is.na(S1)==T)], 
+     main="Distribution of elevation values", 
+     xlab="Modified Raster, Cell Values",
+     ylab="Relative Frequency (%)",
      col= "purple")
+# length(S1[which(!is.na(S1)==T)])
 
 Cell_size = 10 # A cell is regarded as a square land of 100 sq.m area (10mx10m)
 NumRow = nrow(S1) # Number of cells in a row 
@@ -147,7 +213,6 @@ vector_of_NoDATA = rep(NA,S1_Number_of_NoDATA_cells) # vector of NA's
 ## 4-2) Calculation of key variables for computing Overall Accuracy,
 ## Kappa-statisitcs, Conditional Kappa-statisitcs, and
 ## F-Overlapping statistics
-# S = S1+S2 
 S = S1
 n0_S1dry_AND_S2dry = as.numeric(length(which(S==0))) # S1 and S2 are both dry
 n1_S1wet_AND_S2dry = as.numeric(length(which(S==1))) # S1 is wet and S2 is dry
@@ -192,7 +257,7 @@ n2_S1dry_AND_S2wet = length(which(S==2)) # S1 is dry and S2 is wet
 
 ### Z-statistics computed for Conditional Kappa
 m = matrix(0,ncol=NumCol,nrow=NumRow)
-partition_frame_size = 790 # number of cell in a row or column
+partition_frame_size = 780 # number of cell in a row or column
 Kappa_hat_statistics_i = matrix(NA,
                                 nrow=(ncol(m)-partition_frame_size+1)*
                                   (nrow(m)-partition_frame_size+1),
@@ -246,7 +311,10 @@ print(pvalue) # if pvalue > 0.05, then Kappa is statistically equal to 1 (i.e. N
 # otherwise there is not enough evidence to accept it to be 1
 
 ### 4-3) Visual comparison of "Flood Model 1" vs "Flood Model 2"
-pdf("mygraph.pdf",paper = "USr",width = 10,height = 10)
+
+model1="RAS2D"
+model2="HAND"
+pdf(paste0("ModelComparisonR/",model1,"VS",model2,".pdf"),paper = "USr",width = 10,height = 10)
 #add a color map with 5 colors
 col=c("#EEE9BF","#00EEEE","#F08080","#1874CD","#1874CD")
 #add breaks to the colormap (6 breaks = 5 segments)
@@ -264,10 +332,10 @@ par(xpd = TRUE)
 #add a legend - but make it appear outside of the plot 
 legend( par()$usr[1]+500, #x-coordinate of upper-left corner of legend box
         par()$usr[4]-500, #y-coordinate of upper-left corner of legend box
-        legend = c("Dry Zone", 
-                   expression(paste("Model1 ", intersect(" ") ,"(Model1 ",union(" "), "Model2)")), 
-                   expression(paste("Model2 ",intersect(" ") ,"(Model1 ",union(" "),"Model2)")), 
-                   expression(paste("Model1 ",intersect(" ") ,"Model2"))),
+        legend = c("Dry Zone",
+                            eval(bquote(expression(paste(.(model1)," ", intersect(" "),"(",.(model1)," ",union(" "), .(model2),")")))),
+                            eval(bquote(expression(paste(.(model2)," ",intersect(" "),"(",.(model1)," ",union(" "),.(model2),")")))),
+                            eval(bquote(expression(paste(.(model1)," ",intersect(" "),.(model2)))))),
         bty = "n",fill = col,cex=1)
 legend( par()$usr[2]-4500, #x-coordinate of upper-left corner of legend box
         par()$usr[3]+2500, #y-coordinate of upper-left corner of legend box
@@ -287,7 +355,7 @@ dev.off()
 #############################################################
 ## 5) 
 #### Assessment over depth rasters
-
+options(warn = -1)
 ### 5-1) Intersect of flood raster layers 
 DEM_FloodDepthIntersect = DEM_Flood1-DEM_Flood2
 ### 5-2) Unique flooded zones by a flood raster 1 while it is not flooded by flood raster 2,
@@ -299,17 +367,27 @@ DEM_FloodDepthUnion =
   merge(DEM_FloodDepthIntersect, 
         DEM_FloodDepthClassified_1, 
         DEM_FloodDepthClassified_2)
-### 5-5) write to a new geotiff file (depends on rgdal)
-writeRaster(DEM_FloodDepthIntersect, filename="test_intersct.tif", format="GTiff", overwrite=TRUE)
-writeRaster(DEM_FloodDepthClassified_1, filename="test_1.tif", format="GTiff", overwrite=TRUE)
-writeRaster(DEM_FloodDepthClassified_2, filename="test_2.tif", format="GTiff", overwrite=TRUE)
+options(warn = oldw)
 
+### 5-5) write to a new geotiff file (depends on rgdal)
+
+writeRaster(DEM_FloodDepthIntersect, 
+            filename=paste0("ModelComparisonR/",model1,"VS",model2,"_intersect",".tif"),
+            format="GTiff", overwrite=TRUE)
+writeRaster(DEM_FloodDepthClassified_1, 
+            filename=paste0("ModelComparisonR/",model1,".tif"), 
+            format="GTiff", overwrite=TRUE)
+writeRaster(DEM_FloodDepthClassified_2, 
+            filename=paste0("ModelComparisonR/",model2,".tif"), 
+            format="GTiff", overwrite=TRUE)
+
+par(mfrow=c(1,2))
+plot(DEM_FloodDepthIntersect,col=rev(rainbow(10)))
+plot(DEM_FloodDepthClassified_1,add=T,legend=F,col=rev(heat.colors(10)))
+plot(DEM_FloodDepthClassified_2,add=T,legend=F,col=rev(bpy.colors(10)))
 hist(DEM_FloodDepthIntersect, main="Distribution of elevation values", 
-     col= "purple", 
-     maxpixels=ncell(DEM_FloodDepthIntersect))
+     col= "purple",
+     maxpixels=ncell(DEM_FloodDepthIntersect),prob=T)
 #############################################################
 
-plot(DEM_FloodDepthUnion)
-plot(DEM_FloodDepthIntersect)
-plot(DEM_FloodDepthClassified_1)
-plot(DEM_FloodDepthClassified_2)
+
